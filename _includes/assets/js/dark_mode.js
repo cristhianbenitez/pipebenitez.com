828cf775025a96797b76ae9670565297df57fc94
Cristhian Benitez <cristhiantilleria@gmail.com>
2025-05-01 10:19:08 -0400

    refactor: set default theme to be light and not rely on system preferences, this is a design choice

--- _includes/assets/js/dark_mode.js
+++ _includes/assets/js/dark_mode.js
@@ -1,39 +1,33 @@
 /* Test early for local storage color scheme value to avoid FOIT */
-const currentColorscheme = localStorage.getItem("halide-color-scheme");
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-let isDark;
+const currentColorscheme = localStorage.getItem('halide-color-scheme');
+let isDark = false; // Initialize to false for light default
 
 /* 1. is color preference already saved in local storage? */
-if (currentColorscheme == "dark" || !currentColorscheme && prefersDarkScheme.matches) {
-  document.documentElement.setAttribute("dark", true);
+if (currentColorscheme === 'dark') {
+  // Only check for explicit dark preference
+  document.documentElement.setAttribute('dark', true);
   isDark = true;
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-
+window.addEventListener('DOMContentLoaded', (event) => {
   // Header color scheme toggle (light/dark modes)
-  const csToggle = document.querySelector(".dark-toggle");
+  const csToggle = document.querySelector('.dark-toggle');
 
   if (isDark) {
     csToggle.checked = true;
   }
 
   if (csToggle) {
+    csToggle.addEventListener('change', () => {
+      document.documentElement.toggleAttribute('dark');
 
-    csToggle.addEventListener("change", () => {
+      let cs = 'light';
 
-      document.documentElement.toggleAttribute("dark");
-
-      let cs = "light";
-
-      if (document.documentElement.hasAttribute("dark")) {
-        cs = "dark";
+      if (document.documentElement.hasAttribute('dark')) {
+        cs = 'dark';
       }
 
-      localStorage.setItem("halide-color-scheme", cs);
-
+      localStorage.setItem('halide-color-scheme', cs);
     });
-
   }
-
 });
